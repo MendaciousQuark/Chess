18d12f0a08b440a90e6e3fd09aa3866ce9a181f6
Jonathan Claussnitzer <ed20jicc@leeds.ac.uk>
2023-07-29 14:21:02 +0100

    Essentially finished testMovement in PawnTests

--- src/Tests/PawnTests.java
+++ src/Tests/PawnTests.java
@@ -5,20 +5,120 @@ import java.util.Arrays;
 
 public class PawnTests
 {
+  private Board board;
 
   @BeforeEach
   public void setup()
   {
-    Board board = new Board();
+    board = new Board();
   }
 
   @Test
-  public void testMovement()
+  public void testMovement() {
+    // Set up the board with a specific initial position
+    board.setBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+
+    // Get the specific pawn from the board (row 1, column 3)
+    Pawn pawn = (Pawn) board.getSquare(1, 3).piece;
+
+    // Find possible moves for the pawn
+    pawn.findMoves(board, 1, false);
+
+    // Set the start and end positions for the first move
+    int[] start = setStart(1, 3);
+    int[] end = setEnd(2, 3);
+    // Create the expected move object for the first move
+    Move move1 = new Move(board, start, end, pawn, true, 1, false, false, false, false);
+
+    // Set the start and end positions for the second move
+    start = setStart(1, 3);
+    end = setEnd(3, 3);
+    // Create the expected move object for the second move
+    Move move2 = new Move(board, start, end, pawn, true, 1, false, false, false, false);
+
+    // Assertions for the first pawn's moves
+    Assertions.assertEquals(2, pawn.moves.size());
+    Assertions.assertTrue(pawn.moves.contains(move1));
+    Assertions.assertTrue(pawn.moves.contains(move2));
+
+    // Get another pawn from the board (row 6, column 0)
+    pawn = (Pawn) board.getSquare(6, 0).piece;
+
+    // Find possible moves for the second pawn
+    pawn.findMoves(board, 1, false);
+
+    // Set the start and end positions for the third move
+    start = setStart(6, 0);
+    end = setEnd(5, 0);
+    // Create the expected move object for the third move
+    Move move3 = new Move(board, start, end, pawn, true, 1, false, false, false, false);
+
+    // Set the start and end positions for the fourth move
+    start = setStart(6, 0);
+    end = setEnd(4, 0);
+    // Create the expected move object for the fourth move
+    Move move4 = new Move(board, start, end, pawn, true, 1, false, false, false, false);
+
+    // Assertions for the second pawn's moves
+    Assertions.assertEquals(2, pawn.moves.size());
+    Assertions.assertTrue(pawn.moves.contains(move3));
+    Assertions.assertTrue(pawn.moves.contains(move4));
+
+    // Set up a new board with a specific position
+    board.setBoard("rnbqkbnr/1pp2ppp/3p4/p3p3/1PPP4/8/P3PPPP/RNBQKBNR");
+
+    // Get another pawn from the board (row 3, column 3)
+    pawn = (Pawn) board.getSquare(3, 3).piece;
+
+    // Find possible moves for the third pawn
+    pawn.findMoves(board, 1, false);
+
+    // Set the start and end positions for the fifth move
+    start = setStart(3, 3);
+    end = setEnd(4, 3);
+    // Create the expected move object for the fifth move
+    Move move5 = new Move(board, start, end, pawn, true, 1, false, false, false, false);
+
+    // Set the start and end positions for the sixth move
+    start = setStart(3, 3);
+    end = setEnd(5, 3);
+    // Create the expected move object for the sixth move
+    Move move6 = new Move(board, start, end, pawn, true, 1, false, false, false, false);
+
+    // Assertions for the third pawn's moves
+    Assertions.assertEquals(2, pawn.moves.size());
+    Assertions.assertTrue(pawn.moves.contains(move5));
+    Assertions.assertFalse(pawn.moves.contains(move6));
+
+    // Get another pawn from the board (row 4, column 0)
+    pawn = (Pawn) board.getSquare(4, 0).piece;
+
+    // Find possible moves for the fourth pawn
+    pawn.findMoves(board, 1, false);
+
+    // Set the start and end positions for the seventh move
+    start = setStart(4, 0);
+    end = setEnd(5, 0);
+    // Create the expected move object for the seventh move
+    Move move7 = new Move(board, start, end, pawn, true, 1, false, false, false, false);
+
+    // Set the start and end positions for the eighth move
+    start = setStart(4, 0);
+    end = setEnd(6, 0);
+    // Create the expected move object for the eighth move
+    Move move8 = new Move(board, start, end, pawn, true, 1, false, false, false, false);
+
+    // Assertions for the fourth pawn's moves
+    Assertions.assertEquals(2, pawn.moves.size());
+    Assertions.assertTrue(pawn.moves.contains(move7));
+    Assertions.assertFalse(pawn.moves.contains(move8));
+  }
+
+  @Test
+  public void testCapture()
   {
-  }
 
-  @Test
-  public void testCapture(){}
+  }
 
   @Test
   public void testEnPassant(){}
@@ -26,5 +126,24 @@ public class PawnTests
   @Test
   public void testChained(){}
 
+  private int[] setStart(int i, int j) {
+    // Create a new integer array representing the start position
+    int[] start = new int[2];
+    // Set the row and column values for the start position
+    start[0] = i;
+    start[1] = j;
+    // Return the start position array
+    return start;
+  }
+
+  private int[] setEnd(int i, int j) {
+    // Create a new integer array representing the end position
+    int[] end = new int[2];
+    // Set the row and column values for the end position
+    end[0] = i;
+    end[1] = j;
+    // Return the end position array
+    return end;
+  }
 
 }
